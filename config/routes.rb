2817dd6e610ee10175ff437bf9cde49f82e20e7f
Yony <arleybri18@gmail.com>
2018-11-17 21:04:37 -0500

    Implementa la funcionalidad de salir

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   #rutas para la session
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-
+  delete 'logout', to: 'sessions#destroy'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
